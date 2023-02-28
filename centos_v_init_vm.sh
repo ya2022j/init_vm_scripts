24d204656bdb7181c0e0abc802a2a2c73bbb3092
ya2022j <62124198+ya2022j@users.noreply.github.com>
2023-02-28 13:18:18 +0900

    Update centos_v_init_vm.sh

--- centos_v_init_vm.sh
+++ centos_v_init_vm.sh
@@ -252,3 +252,42 @@ wget  https://go.dev/dl/go1.19.3.linux-amd64.tar.gz;
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.3.linux-amd64.tar.gz;
 export PATH=$PATH:/usr/local/go/bin;
 go version;
+
+
+
+clear
+echo
+echo "#############################################################"
+echo "#install nginx                                              #"
+echo
+
+
+
+cd /usr/local/src;
+wget https://nginx.org/download/nginx-1.23.1.tar.gz;
+
+
+
+tar -zxvf nginx-1.23.1.tar.gz;
+cd nginx-1.23.1;
+#mkdir /usr/local/ssl;
+./configure --prefix=/usr/local/nginx --with-http_stub_status_module --with-http_ssl_module;
+make && make install
+
+#启动
+#/usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf
+#
+#
+#或者强制停止
+#
+#pkill -9 nginx
+#
+#重启：
+#
+#在启动命令-c前加-t
+#
+#/usr/local/nginx/sbin/nginx -t -c /usr/local/nginx/conf/nginx.conf
+#
+
+
+
