f84d175c0d8e3712869bb7294b2c2df5c0747f9a
ya2022j <62124198+ya2022j@users.noreply.github.com>
2023-10-10 09:11:57 +0900

    Update centos_v_init_vm.sh

--- centos_v_init_vm.sh
+++ centos_v_init_vm.sh
@@ -288,7 +288,9 @@ wget https://nginx.org/download/nginx-1.23.1.tar.gz;
 tar -zxvf nginx-1.23.1.tar.gz;
 cd nginx-1.23.1;
 #mkdir /usr/local/ssl;
-./configure --prefix=/usr/local/nginx --with-http_stub_status_module --with-http_ssl_module;
+#./configure --prefix=/usr/local/nginx --with-http_stub_status_module --with-http_ssl_module;
+./configure --prefix=/usr/local/nginx --with-http_stub_status_module  --with-openssl=/usr/local/openssl ;
+
 make && make install
 
 #启动
